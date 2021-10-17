6f2090a0d3aac5c619659a4d1578c59945ec0dd5
kevindar <kevindar2705@gmail.com>
2021-10-17 18:28:04 +0700

    Finished vaccine counter

--- VaccineCount.java
+++ VaccineCount.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.regex.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -9,21 +10,28 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
 public class VaccineCount {
     public static class VaccineCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+        // initialize the field variable
         private final static IntWritable one = new IntWritable(1);
+        private final static int COLUMN_USED = 11;  //contains hashtags
+        private final static String CSV_SEPARATOR = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
         private Text word = new Text();
-        
-        public void map(Object key, Text value, Context context) throw IOException, InterruptedException 
-        {
-            String[] row = value.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-            
-            String hashtag = row[11];
 
-            for (String str: hashtag)
+        public void map(Object key, Text value, Context context)
+        throws IOException, InterruptedException 
+        {
+
+            String[] columnData = value.toString().split(CSV_SEPARATOR);
+            Pattern p = Pattern.compile("\\[[^\\[\\]]+\\]");
+
+            for (String hashtags: columnData)
             {
-                word.set(str);
-                context.write(word, one);
+                if(p.matcher(hashtags).matches()){
+                    word.set(hashtags);
+                    context.write(word, one);
+                }
             }
         }
     }
@@ -31,7 +39,7 @@ public class VaccineCount {
     public static class VaccineCountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
         public void reduce(Text key, Iterable<IntWritable> values,
             Context context
-            ) throw IOException, InterruptedException {
+            ) throws IOException, InterruptedException {
                 int total = 0;
                 for (IntWritable val : values) {
                 total++ ;
@@ -40,7 +48,7 @@ public class VaccineCount {
         }
     }
     
-    public static void main(String[] args) throw Exception {
+    public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
         Job job = new Job(conf, "word count");
         job.setJarByClass(VaccineCount.class);
